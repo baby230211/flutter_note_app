b191c1bfd22d88e44baf46cb1fb3cf016aba92a4
SeanHsieh <t4612310@gmail.com>
2022-12-04 23:01:17 +0800

    step-13

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:my_app/constants/routes.dart';
 import 'package:my_app/services/auth/auth_service.dart';
 import 'package:my_app/views/login_view.dart';
-import 'package:my_app/views/notes_view.dart';
+import 'package:my_app/views/notes/new_note_view.dart';
+import 'package:my_app/views/notes/notes_view.dart';
 import 'package:my_app/views/register_view.dart';
 import 'package:my_app/views/verify_email_view.dart';
 import 'dart:developer' as devtools show log;
@@ -19,7 +20,8 @@ void main() {
       notesRoute: (context) => const NotesView(),
       loginRoute: (context) => const LoginView(),
       registerRoute: (context) => const RegisterView(),
-      verifyEmailRoute: (context) => const VerifyEmailView()
+      verifyEmailRoute: (context) => const VerifyEmailView(),
+      newNoteRoute: (context) => const NewNoteView(),
     },
   ));
 }
