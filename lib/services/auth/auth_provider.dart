94567a580f054782643e238d5485d28bc50d26e3
SeanHsieh <t4612310@gmail.com>
2022-12-04 11:27:46 +0800

    step-8

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,19 @@
+import 'package:my_app/services/auth/auth_user.dart';
+
+// return AuthUser
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+
+  Future<void> logOut();
+
+  Future<void> sendEmailVerification();
+}
